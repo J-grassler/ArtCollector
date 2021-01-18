d6567bb29fa8cd61df882350d383933e8ddefbd5
J-grassler <72049255+J-grassler@users.noreply.github.com>
2021-01-18 17:31:36 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,225 @@
+const BASE_URL = 'https://api.harvardartmuseums.org';
+const KEY = 'apikey=e414e7cf-4ac4-49dd-93aa-fedceb138881';
+
+function onFetchStart() {
+  $('#loading').addClass('active');
+};
+
+function onFetchEnd() {
+  $('#loading').removeClass('active');
+};
+
+async function fetchObjects() {
+  const url = `${ BASE_URL }/object?${ KEY }`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;  
+    console.log(data);
+
+  } catch (error) {
+    console.error(error);
+  };
+};
+
+async function fetchAllCenturies() {
+  const url = `${ BASE_URL }/century?${ KEY }&size=100&sort=temporalorder`;
+  if (localStorage.getItem('centuries')) {
+      return JSON.parse(localStorage.getItem('centuries'));
+  };
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const records = data.records;
+    localStorage.setItem('centuries',JSON.stringify(records));
+    return records;
+  } catch (error) {
+    console.error(error);
+  };
+};
+
+async function fetchAllClassifications() {
+  const url = `${ BASE_URL }/classification?${ KEY }&size=100&sort=name`;
+  
+  if (localStorage.getItem('classifications')) {
+      return JSON.parse(localStorage.getItem('classifications'));
+  };  
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const records = data.records;
+    localStorage.setItem('classifications', JSON.stringify(records));
+    return records;
+  } catch (error) {
+      console.error(error);
+  }; 
+};
+
+async function prefetchCategoryLists() {
+  try {
+    const [classifications, centuries] = await Promise.all([
+       fetchAllClassifications(),
+       fetchAllCenturies()
+      ]);
+     
+      $('.classification-count').text(`(${classifications.length})`);
+      classifications.forEach(classification => {
+          $('#select-classification').append($(`<option value="${classification.name}">${classification.name}</option>`));
+      });
+      $('.century-count').text(`(${centuries.length})`);
+      centuries.forEach(century => {          
+          $('#select-century').append($(`<option value="${century.name}">${century.name}</option>`));
+      });
+  } catch (error) {
+      console.error(error);
+  };
+};
+
+function buildSearchString() {
+  const classify = $('#select-classification').val()
+  const centure = $('#select-century').val()
+  const keyWords = $('#keywords').val()
+  const url = `${BASE_URL}/object?${KEY}&classification=${classify}&century=${centure}&keyword=${keyWords}`;
+  return url;
+};
+
+function renderPreview(record) {
+  const { primaryimageurl, title} = record;
+  return $(`<div class="object-preview">
+    <a href="#">
+      ${primaryimageurl ? `<img src="${primaryimageurl}" />` : ""}
+      ${title ? `<h3>${title}</h3>` : ""}
+    </a>
+  </div>`).data('record', record);
+};
+
+function updatePreview(records, info) {
+  const root = $('#preview');
+  if (info.next) {
+      $('.next').data('url', info.next).attr('disabled', false)
+  } else {
+      $('.next').data('url', null).attr('disabled', true)
+  };
+  if (info.prev) {
+      $('.prev').data('url', info.prev).attr('disabled', false)
+  } else {
+      $('.prev').data('url', null).attr('disabled', true)
+  };
+  const results = root.find('.results');
+  results.empty();
+  results.append(records.map(renderPreview));
+};
+
+function photosHTML(images, primaryimageurl) {
+  if (images && images.length > 0 ){
+    return images.map(function(image){
+      return `<img src="${ image.baseimageurl }" />`});
+
+  } else if ( primaryimageurl){
+    return `<img src = "${primaryimageurl}"/>`;  
+  } else {
+    return "";
+  };
+};
+
+function renderFeature(record) {
+  const {title, dated, description, culture, style, technique, medium, dimensions, people, department, division, contact, creditline ,images, primaryimageurl} = record;
+ return $(` <div class="object-feature">
+        <header>
+        <h3>${ title }<h3>
+        <h4>${ dated }</h4>
+        </header>
+        <section class="facts">
+        ${ factHTML("Description", description,) }
+        ${ factHTML("Culture", culture, 'culture') }
+        ${ factHTML("Technique", technique, 'technique') }
+        ${ factHTML("Medium", medium, 'medium') }
+        ${ factHTML("Dimensions", dimensions) }
+        ${record.people ? record.people.map(function(person) {
+              return factHTML('Person', person.displayname, 'person');
+              }).join(''):''}
+        ${ factHTML("Department", department) }
+        ${ factHTML("Division", division) }
+        ${ factHTML('Contact', `<a target="_blank" href="mailto:${ contact }">${ contact }</a>`) }
+        ${ factHTML("Credit Line", creditline) }
+        </section>
+        <section class="photos">
+        ${ photosHTML(record.images, record.primaryimageurl) }
+        </section>
+        </div>`);
+};
+
+function searchURL(searchType, searchString) {
+    return `${ BASE_URL }/object?${ KEY }&${ searchType}=${ searchString }`;
+};
+  
+function factHTML(title, content, searchTerm = null) {
+  if (!content){
+      return '';
+    } 
+    return `<span class="title">${ title }</span>
+    <span class="content">${searchTerm && content ? `<a href="${searchURL(content, searchTerm)}${encodeURI(content)}">${content}</a>`: content}
+    </span>`
+};
+
+$('#search').on('submit', async function (event) {
+  event.preventDefault();
+  onFetchStart();
+  try {
+      const encodeUrl = encodeURI(buildSearchString())
+      const response = await fetch(encodeUrl);
+      const {info, records} = await response.json();
+      console.log(info, records)
+      updatePreview(records, info);
+  } catch (error) {
+      console.error(error);
+  } finally {
+      onFetchEnd();
+  };
+});
+
+$('#preview .next, #preview .previous').on('click', async function () {
+  onFetchStart();
+  try {
+      let url = $(this).data('url')
+      const response = await fetch(url);
+      const {info, records }= await response.json();
+      updatePreview(records, info);
+  } catch (error) {
+      console.error(error);
+  } finally {
+      onFetchEnd();
+  };        
+});
+
+$('#preview').on('click', '.object-preview', function (event) {
+  event.preventDefault();
+  const objectPreview = $(this).closest('.object-preview');
+  const objectPreviewData = objectPreview.data('record');
+  console.log(objectPreviewData);
+  $('#feature').html(renderFeature(objectPreviewData));
+});
+
+$('#feature').on('click', 'a', async function (event) {
+  const url = $(this).attr('href');
+  if (url.startsWith('mailto')) { return; };
+  event.preventDefault();
+  console.log(url);
+  onFetchStart();
+  try {
+  const response = await fetch(url);
+  const { info, records } = await response.json();
+  updatePreview(records, info);
+  } catch (error) {
+  console.error(error);
+  }finally {
+    onFetchEnd();
+  };
+});  
+
+function bootstrap(){
+    fetchObjects();
+    prefetchCategoryLists();
+};
+bootstrap();
